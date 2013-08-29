872d7a646f182ce5f7a3b1872e76a812d24d8bf0
Nicolas Maubourguet <nicolas@kiwixi.com>
2013-08-29 11:49:26 +0200

    changing some template on editor and elementId on the view.

--- app/views/editor.js
+++ app/views/editor.js
@@ -1,8 +1,8 @@
 var EditorView = Ember.View.extend({
     didInsertElement:function(){
         console.log('slide view inserted');
-    }
-
+    },
+    elementId:"editor"
 });
 
 export default EditorView;
