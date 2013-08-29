213a1c97b1da02212a496935165635d12ee96d9d
Nicolas Maubourguet <nicolas@kiwixi.com>
2013-08-29 11:57:16 +0200

    editor improvement to handle add/select/delete

--- app/controllers/editor.js
+++ app/controllers/editor.js
@@ -1,9 +1,19 @@
 import Slide from '../models/slide';
 
 var EditorController = Ember.ArrayController.extend({
-  //content: Ember.A(),
-  addSlide : function(){
+  //the current selected slide
+  currentSlide:Ember.K(),
+  //action to add a Slide
+  addSlideAction : function(){
     this.pushObject(Slide.create());
+  },
+  //action to select the slide
+  selectSlideAction:function(event){
+    console.log(event);
+  },
+  //action to handle the deletion event
+  deleteSlideAction : function(event){
+    this.removeObject(event);
   }
 });
 
