3f4aaaca4a8ff012da66634d0e1a4d3e768d8e42
Stefan Fochler <stefan@fochler.de>
2013-08-29 12:32:59 +0200

    Add toggleSelected that proxies to editorController

--- app/controllers/slide.js
+++ app/controllers/slide.js
@@ -1,4 +1,10 @@
-var SlideController = Ember.ObjectController.extend();
+var SlideController = Ember.ObjectController.extend({
+  needs: ['editor'],
+
+  toggleSelected: function() {
+    this.get('controllers.editor').selectSlideAction(this.get('content'));
+  }
+});
 
 
 export default SlideController;
